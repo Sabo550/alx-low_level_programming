189a8500c8e63b04a6e3205c448126f76bebf38e
Sabo550 <saborahmatu866@gmail.com>
2023-05-02 14:17:56 -0700

    0x013-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 
 /**
- * sum_listint - the returns of the sum of all data (n) of
- * linked list.
+ * sum_listint - returns sum of all data (n) of
+ * listint_t list.
  * @head: the head of list.
  *
- * Return: the sum of data (n).
+ * return: if the list is empty - 0.
+ *      otherwise- sum of all the data (n).
  */
-integer sum_listint(listint_t *head)
+int sum_listint(listint_t *head)
 {
-	integer sum;
+	int sum;
 
 	sum = 0;
 	while (head != NULL)
